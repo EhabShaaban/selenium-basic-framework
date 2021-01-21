dfcf6d68944d99c2d6c90dae2a4ecc70cd7d8c43
EhabShaaban <ehabsfeki@gmail.com>
2021-01-21 18:36:27 +0200

    Add EventFiringWebDriver

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManger;
 
 import java.io.File;
@@ -19,11 +21,18 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTests {
     private WebDriver driver;
+    /*
+    * EventFiringWebDriver for executing functions before specific behavior
+    * */
+//    private EventFiringWebDriver driver;
     protected HomePage homePage;
     String url = "https://the-internet.herokuapp.com/";
+
     @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver");
+        /*driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());*/
         driver = new ChromeDriver();
         //driver.manage().window().setSize(new Dimension(375, 812));
         //driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
